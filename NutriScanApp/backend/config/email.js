8ebf3c4b538f15a7015b1ee19576572b932915a4
tamayogarciajesusantonio0-code <tamayogarciajesusantonio0@gmail.com>
2026-07-20 22:30:12 -0600

    cambiar puerto SMTP a 587

--- NutriScanApp/backend/config/email.js
+++ NutriScanApp/backend/config/email.js
@@ -6,7 +6,9 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_FROM,
     pass: process.env.GMAIL_APP_PASSWORD
